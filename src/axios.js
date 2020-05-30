5e965eb368e5fb656cc997643541070cb1be6df9
Dang Chi <chid@jobready.com.au>
2020-05-30 09:07:02 +0700

    Creating and Using Axios Instance, Axios Interceptor

---
+++ src/axios.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+
+});
+
+instance.interceptors.request.use(request => {
+  console.log('success:',request);
+  return request;
+}, error => {
+  console.log('error:', error);
+  return Promise.reject(error);
+});
+
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
+
+export default instance;
